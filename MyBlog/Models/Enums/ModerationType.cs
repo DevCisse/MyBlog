dcf116cbaab4621f5ad9dba7e9c9491b5d9c64a0
Hassan Salihu <45653783+DevCisse@users.noreply.github.com>
2021-08-27 12:17:14 +0100

    Added template and models

---
+++ MyBlog/Models/Enums/ModerationType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlog.Models.Enums
+{
+    public enum ModerationType
+    {
+        [Description("Political propaganda")]
+        Political,
+        [Description("Offensice language")]
+        Language,
+        [Description("Drug references")]
+        Drugs,
+        [Description("Threatening speech")]
+        Threatening,
+        [Description("Sexual content")]
+        Sexual,
+        [Description("HateSpeech")]
+        HateSpeech,
+        [Description("Targerted shaming")]
+        Shaming
+    }
+}
